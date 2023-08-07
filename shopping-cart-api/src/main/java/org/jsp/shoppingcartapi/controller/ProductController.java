04211eed167f83d19a57d1061fdfb49f63291602
sathishnyadav <sathish.n.yadav@gmail.com>
2023-08-07 18:41:14 +0530

    added the changed product controller

--- shopping-cart-api/src/main/java/org/jsp/shoppingcartapi/controller/ProductController.java
+++ shopping-cart-api/src/main/java/org/jsp/shoppingcartapi/controller/ProductController.java
@@ -7,7 +7,6 @@ import org.jsp.shoppingcartapi.dto.ResponseStructure;
 import org.jsp.shoppingcartapi.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @CrossOrigin
 public class ProductController {
 	@Autowired
@@ -50,6 +50,6 @@ public class ProductController {
 
 	@GetMapping("/products/byCategory/{category}")
 	public ResponseEntity<ResponseStructure<List<Product>>> findProductsByCategory(@PathVariable String category) {
-		return service.findProductsByBrand(category);
+		return service.findProductsByCategory(category);
 	}
 }
